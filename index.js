67415093cca92f57d0cac4bae4449af5a90c28db
destroyer22719 <65511595+destroyer22719@users.noreply.github.com>
2020-08-07 00:44:24 -0400

    added mouse hover icon change on buttons

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const selectorInput = $("#selectorInput");
 const elementsInput = $("#elementsInput");
 
 const elementsButton = $("#addElement")
+const selectorButton = $("#addSelector")
 
 elementsButton.click(() => {
     sandbox.html(elementsInput.val());
