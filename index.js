024a09379d6d23197c72439cbf93b01e87562cf6
Nathan <nathancai1031@gmail.com>
2020-08-07 20:26:10 -0400

    done

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+//sandbox div
 const sandbox = $("#sandbox");
 
+//inputs
 const selectorInput = $("#selectorInput");
 const elementsInput = $("#elementsInput");
 
-const elementsButton = $("#addElement")
-const selectorButton = $("#addSelector")
+//add buttons
+const elementsButton = $("#addElement");
+const selectorButton = $("#addSelector");
 
+//clear buttons
+const clearSelector = $("#clearSelector");
+const clearElement = $("#clearElement");
+
+//add elements
 elementsButton.click(() => {
     sandbox.html(elementsInput.val());
-    console.log(sandbox.html())
+    console.log(sandbox.children().length);
+    
+    //expands height of sandbox when there are enough children elements
+    if(sandbox.children().length >= 3){
+        console.log('adding')
+        sandbox.css("height","+=150px")
+    }
 })
 
+//finds selector elements
 selectorButton.click(() => {
     let selectorFound = sandbox.find(`${selectorInput.val()}`)
     $('#selectorsFound').text(selectorFound.length)
-    console.log(selectorFound.length)// console.log(selectorInput.val())
+    selectorFound.css('border','2px solid green');
 })
 
+//clear buttons
+clearSelector.click(() => {
+    selectorInput.val("")
+})
+clearElement.click(() => {
+    elementsInput.val("")
+    sandbox.empty();
+
+})
+
+//set html of sandbox to elements textarea
 elementsInput.val(sandbox.html())
