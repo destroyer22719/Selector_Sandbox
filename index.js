20de1cd9e329c09a0fc40afd15476551b3b660ea
Nathan <nathancai1031@gmail.com>
2020-10-05 16:07:16 -0400

    testing new workflow

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ elementsButton.click(() => {
     sandbox.height(sandbox.children().length * 50 )
 })
 
+
 //finds selector elements
 selectorButton.click(() => {
     let selectorFound = sandbox.find(`${selectorInput.val()}`)
